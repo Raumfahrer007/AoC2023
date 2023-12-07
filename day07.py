e63f7b43588d204570210b326bffcc090c1e9211
Nicolas Braun <n.braun1011@gmail.com>
2023-12-07 23:32:58 +0100

    Day 07 Part 2

--- day07.py
+++ day07.py
@@ -1,30 +1,30 @@
-card_values = {
-    "A": 12,
-    "K": 11,
-    "Q": 10,
-    "J": 9,
-    "T": 8,
-    "9": 7,
-    "8": 6,
-    "7": 5,
-    "6": 4,
-    "5": 3,
-    "4": 2,
-    "3": 1,
-    "2": 0
-}
-
 hand_values = {
-    "5": 6000000000000,
-    "41": 5000000000000,
-    "32": 4000000000000,
-    "311": 3000000000000,
-    "221": 2000000000000,
-    "2111": 1000000000000,
+    "5": 60000000000,
+    "41": 50000000000,
+    "32": 40000000000,
+    "311": 30000000000,
+    "221": 20000000000,
+    "2111": 10000000000,
     "11111": 0
 }
 
 def part_one(input):
+    card_values = {
+        "A": 12,
+        "K": 11,
+        "Q": 10,
+        "J": 9,
+        "T": 8,
+        "9": 7,
+        "8": 6,
+        "7": 5,
+        "6": 4,
+        "5": 3,
+        "4": 2,
+        "3": 1,
+        "2": 0
+    }
+
     sum = 0
     hands = []
 
@@ -34,7 +34,7 @@ def part_one(input):
 
         hand_count = {}
         hand_number = 0
-        k = 10000000000
+        k = 100000000
         for card in hand:
             hand_number += int(card_values[card]) * k
             k //= 100
@@ -43,9 +43,9 @@ def part_one(input):
             else:
                 hand_count[card] += 1
 
-        sorted_hand_count = "".join(str(card) for card in sorted(hand_count.values(), reverse=True))
+        sorted_hand_count_string = "".join(str(count) for count in sorted(hand_count.values(), reverse=True))
 
-        hand_number += hand_values[sorted_hand_count]
+        hand_number += hand_values[sorted_hand_count_string]
         hands.append([hand_number, int(bid)])
 
     sorted_hands = sorted(hands, key=lambda l:l[0])
@@ -56,7 +56,63 @@ def part_one(input):
     print(f"Sum PartOne: {sum}")
 
 def part_two(input):
-    pass
+    card_values = {
+        "A": 12,
+        "K": 11,
+        "Q": 10,
+        "T": 9,
+        "9": 8,
+        "8": 7,
+        "7": 6,
+        "6": 5,
+        "5": 4,
+        "4": 3,
+        "3": 2,
+        "2": 1,
+        "J": 0,
+    }
+
+    sum = 0
+    hands = []
+
+    for line in input:
+        hand = line.split(" ")[0]
+        bid = line.split(" ")[1]
+
+        hand_count = {}
+        hand_number = 0
+        k = 100000000
+        for card in hand:
+            hand_number += int(card_values[card]) * k
+            k //= 100
+            if not card in hand_count.keys():
+                hand_count[card] = 1
+            else:
+                hand_count[card] += 1
+
+        sorted_hand_count = dict(sorted(hand_count.items(), key=lambda item:item[1], reverse=True))
+
+        if "J" in sorted_hand_count.keys():
+            if not sorted_hand_count["J"] == 5:
+                first_key = list(sorted_hand_count.keys())[0]
+                if first_key == "J":
+                    first_key = list(sorted_hand_count.keys())[1]
+
+                sorted_hand_count[first_key] += sorted_hand_count["J"]
+                del sorted_hand_count["J"]
+
+
+        sorted_hand_count_string = "".join(str(count) for count in sorted_hand_count.values())
+
+        hand_number += hand_values[sorted_hand_count_string]
+        hands.append([hand_number, int(bid)])
+
+    sorted_hands = sorted(hands, key=lambda l:l[0])
+    
+    for i, hand in enumerate(sorted_hands):
+        sum += hand[1] * (i+1)
+
+    print(f"Sum PartTwo: {sum}")
 
 
 test_1 = [
@@ -72,5 +128,5 @@ if __name__ == "__main__":
     input = data.readlines()
     data.close()
 
-    part_one(input)
-    #part_two(input)
+    #part_one(input)
+    part_two(input)
